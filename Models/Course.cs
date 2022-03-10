23ddd2a09da3ec8381e240177d84367b4cecdf43
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-03-10 13:11:53 -0600

    Add AverageGrade property to Courses.

--- Models/Course.cs
+++ Models/Course.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public double AverageGrade { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
-
         public Course()
         {
             Enrollments = new HashSet<Enrollment>();
